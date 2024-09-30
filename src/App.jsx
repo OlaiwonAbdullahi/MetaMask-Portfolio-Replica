246b83d2b75a7e8b964e32a05b06743fc6a340ed
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-30 13:35:50 +0100

    update App.jsx and tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
-export const App = () => {
-  return <div>App</div>;
-};
+export default function App() {
+  return (
+    <div className=" flex items-center justify-center">
+      <div className=" text-4xl text-center ">MetaMask Portfolio Clone</div>
+    </div>
+  );
+}
