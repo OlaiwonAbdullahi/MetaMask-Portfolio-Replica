a721a84f24ac1ddabe0d22f311e122ddefe528c4
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-01 22:12:38 +0100

    update NavBar.jsx

--- src/components/headers/NavBar.jsx
+++ src/components/headers/NavBar.jsx
@@ -3,11 +3,11 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import Button from "../Button";
 import Search from "../Search";
 
-const NavBar = () => {
+const NavBar = ({ children }) => {
   return (
     <div className="flex flex-row justify-between bg-bgColor h-24 border-b-2 border-b-secondary pt-5 px-4">
       <div className="">
-        <h2 className="text-2xl font-bold mt-3">Tokens</h2>
+        <h2 className="text-2xl font-bold mt-3">{children}</h2>
       </div>
       <div className=" flex gap-3 p-1">
         <Search />
