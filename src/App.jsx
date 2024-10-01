59ba4df096e26a3d8111b5e60e2d17a2ab11f277
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-01 21:31:29 +0100

    update 2 files and delete 1 file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Header from "./components/Header";
+import Header from "./components/headers/Header";
+import NavBar from "./components/headers/NavBar";
 
 export default function App() {
   return (
     <div className=" ">
       <div className=" text-4xl text-center font-pop ">
         <Header />
+        <NavBar />
       </div>
     </div>
   );
