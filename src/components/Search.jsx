72ea91713a0e5bf1b109fccf50a61b7a9461b1b5
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-01 21:38:21 +0100

    update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,10 @@
+import { IoIosSearch } from "react-icons/io";
 const Search = () => {
   return (
     <div className="rounded-md">
-      <div className=""></div>
+      <div className="">
+        <IoIosSearch />
+      </div>
       <input type="text" placeholder="Search For a token" />
     </div>
   );
