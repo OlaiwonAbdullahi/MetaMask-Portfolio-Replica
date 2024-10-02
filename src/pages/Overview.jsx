3cfa752151fb1b1041b7017b7a81c666ff9466f6
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-02 16:27:12 +0100

    update Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,12 +1,14 @@
 import SideBar from "../components/Navigations/SideBar";
 import Header from "../components/Navigations/Header";
+
 const Overview = () => {
   return (
-    <div className="bg-bgColor flex">
-      <div className=" basis-1/4">
+    <div className="bg-bgColor flex flex-row gap-4">
+      <div className="basis-1/4 md:basis-1/6">
         <SideBar />
       </div>
-      <div className="basis-3/4 md:basis-">
+
+      <div className="basis-3/4 md:basis-5/6">
         <Header />
       </div>
     </div>
