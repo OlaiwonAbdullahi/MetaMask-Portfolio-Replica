2b8e1381165606314b3b49b0c638a294c6f4a781
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-02 16:45:02 +0100

    update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,21 @@ import heroimg1 from "../assets/heroimg1.webp";
 import heroimg2 from "../assets/heroimg2.webp";
 const Hero = () => {
   return (
-    <div>
-      <div className="">
+    <div className=" flex justify-between">
+      <div>
         <img src={heroimg1} alt="" />
       </div>
-      <div className="">type shii</div>
+      <div className="">
+        <div className="">
+          <h2>Get started with MetaMask Portfolio</h2>
+          <p>
+            Get a clear, comprehensive view of your accounts and wallets, across
+            all networks. Connect a MetaMask wallet or watch any Ethereum
+            address.
+          </p>
+        </div>
+        <div className=""></div>
+      </div>
       <div className="">
         <img src={heroimg2} alt="" />
       </div>
