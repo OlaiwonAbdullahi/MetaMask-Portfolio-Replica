68fe4b86a69c93f2f800a5c81de3684d7041a1f7
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-02 21:03:49 +0100

    update Feature.jsx

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -4,35 +4,39 @@ import Defi from "../assets/defi.jpg";
 import Button from "./Button";
 const Feature = () => {
   return (
-    <div className=" flex border bg-bgColor pt-3">
-      <div className="">
-        <img src={Ramp} alt="" className="" />
-        <h2>Easily on-ramp or off-ramp</h2>
-        <p>
+    <div className=" flex border bg-bgColor">
+      <div className=" border p-3 w-full text-center flex flex-col items-center gap-4">
+        <img src={Ramp} alt="" className="w-11/12" />
+        <h2 className="text-2xl font-bold">Easily on-ramp or off-ramp</h2>
+        <p className="text-sm">
           Convert cash to crypto or crypto for cash while maintaining control of
           your assets, no centralized exchange necessary.
         </p>
         <Button>Buy Crypto</Button>
       </div>
 
-      <div className="">
-        <img src={Move} alt="" />
-        <h2>Easily on-ramp or off-ramp</h2>
-        <p>
+      <div className=" border p-3 w-full text-center flex flex-col items-center gap-4">
+        <img src={Move} alt="" className="w-11/12" />
+        <h2 className="text-2xl font-bold mt-9">
+          Move your crypto almost anywhere
+        </h2>
+        <p className="text-sm">
           Convert cash to crypto or crypto for cash while maintaining control of
           your assets, no centralized exchange necessary.
         </p>
-        <Button>Buy Crypto</Button>
+        <Button>Send Crypto</Button>
       </div>
 
-      <div className="">
-        <img src={Defi} alt="" />
-        <h2>Easily on-ramp or off-ramp</h2>
-        <p>
+      <div className=" border p-3 w-full text-center flex flex-col items-center gap-4">
+        <img src={Defi} alt="" className="w-11/12" />
+        <h2 className="text-2xl font-bold">
+          Diversify your portfolio with DeFi
+        </h2>
+        <p className="text-sm">
           Convert cash to crypto or crypto for cash while maintaining control of
           your assets, no centralized exchange necessary.
         </p>
-        <Button>Buy Crypto</Button>
+        <Button>Stalk Crypto</Button>
       </div>
     </div>
   );
