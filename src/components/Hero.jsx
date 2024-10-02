da96e65ed1b8c458148066f243ccae90582d42ca
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-02 16:36:03 +0100

    update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
+import heroimg1 from "../assets/heroimg1.webp";
+import heroimg2 from "../assets/heroimg2.webp";
 const Hero = () => {
   return (
     <div>
-      <div className="">stuff</div>
+      <div className="">
+        <img src={heroimg1} alt="" />
+      </div>
       <div className="">type shii</div>
-      <div className="">some shii</div>
+      <div className="">
+        <img src={heroimg2} alt="" />
+      </div>
     </div>
   );
 };
