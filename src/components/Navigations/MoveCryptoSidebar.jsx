554a489494d0e7c0909f406ffd0d02e99d7c196f
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-03 13:27:26 +0100

    update MoveCryptoSidebar.jsx

--- src/components/Navigations/MoveCryptoSidebar.jsx
+++ src/components/Navigations/MoveCryptoSidebar.jsx
@@ -8,40 +8,40 @@ import { HiArrowPath } from "react-icons/hi2";
 export default function MoveCrypto() {
   return (
     <div className="">
-      <div className="">
-        <GoPlus className="text-xl text-black h-20" />
+      <div className=" flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
+        <GoPlus className="text-xl text-secondary w-10 h-10" />
         <span>Buy</span>
       </div>
-      <div className="">
+      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
         <FiMinus />
         <span>Sell</span>
       </div>
-      <div className="">
+      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
         <HiArrowPath />
 
         <span>Swap</span>
       </div>
-      <div className="">
+      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
         <PiSwapLight />
         <span>Bridge</span>
       </div>
-      <div className="">
+      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
         <MdOutlineArrowOutward />
         <span>Send</span>
       </div>
-      <div className="">
+      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
         <PiSwapLight />
         <span>Transfer</span>
       </div>
-      <div className="">
+      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
         <FiArrowDownLeft />
-        <span>Transfer</span>
+        <span>Receive</span>
       </div>
-      <div className="">
+      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
         <PiSwapLight />
         <span>Stalk</span>
       </div>
-      <div className="">
+      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
         <PiSwapLight />
         <span>Swap</span>
       </div>
