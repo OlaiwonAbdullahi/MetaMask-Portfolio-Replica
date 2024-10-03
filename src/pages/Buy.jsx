a2f29b5edf52ef7da8d2164c2d85218b32ee3f7b
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-03 17:54:13 +0100

    update Buy.jsx

--- src/pages/Buy.jsx
+++ src/pages/Buy.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import SideBar from "../components/Navigations/SideBar";
 import NavBar from "../components/Navigations/NavBar";
-import Hero from "../components/Hero";
-import Feature from "../components/Feature";
-import Explore from "../components/Explore";
+
 import MoveCrypto from "../components/Navigations/MoveCryptoSidebar";
 
 const Buy = () => {
@@ -25,9 +23,6 @@ const Buy = () => {
 
       <div className="basis-3/4 md:basis-5/6">
         <NavBar>Buy</NavBar>
-
-        <Feature />
-        <Explore />
       </div>
     </div>
   );
