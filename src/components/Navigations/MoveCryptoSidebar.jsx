3b7c62173f05ffe740543656fc22b9b744859e4a
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-03 16:06:36 +0100

    update MoveCryptoSidebar.jsx and SideBar.jsx

--- src/components/Navigations/MoveCryptoSidebar.jsx
+++ src/components/Navigations/MoveCryptoSidebar.jsx
@@ -1,53 +1,31 @@
-import { GoPlus } from "react-icons/go";
-import { FiMinus } from "react-icons/fi";
-import { PiSwapLight } from "react-icons/pi";
+import { GoPlus, GoLink } from "react-icons/go";
+import { FiMinus, FiArrowDownLeft } from "react-icons/fi";
+import { PiSwapLight, PiPlantLight } from "react-icons/pi";
 import { MdOutlineArrowOutward } from "react-icons/md";
-import { FiArrowDownLeft } from "react-icons/fi";
-import { GoLink } from "react-icons/go";
-import { PiPlantLight } from "react-icons/pi";
 import { VscLoading } from "react-icons/vsc";
-
 import { HiArrowPath } from "react-icons/hi2";
+
+const ActionButton = ({ Icon, label }) => {
+  return (
+    <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
+      <Icon className="text-xl text-text w-6 h-6" />
+      <span>{label}</span>
+    </div>
+  );
+};
+
 export default function MoveCrypto() {
   return (
-    <div className="">
-      <div className=" flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <GoPlus className="text-xl text-secondary w-6 h-6" />
-        <span>Buy</span>
-      </div>
-      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <FiMinus className="text-xl text-secondary w-6 h-6" />
-        <span>Sell</span>
-      </div>
-      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <HiArrowPath />
-
-        <span>Swap</span>
-      </div>
-      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <VscLoading />
-        <span>Bridge</span>
-      </div>
-      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <MdOutlineArrowOutward />
-        <span>Send</span>
-      </div>
-      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <GoLink />
-        <span>Transfer</span>
-      </div>
-      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <FiArrowDownLeft />
-        <span>Receive</span>
-      </div>
-      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <PiPlantLight />
-        <span>Stalk</span>
-      </div>
-      <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-        <PiSwapLight />
-        <span>Swap</span>
-      </div>
+    <div className="font-medium">
+      <ActionButton Icon={GoPlus} label="Buy" />
+      <ActionButton Icon={FiMinus} label="Sell" />
+      <ActionButton Icon={HiArrowPath} label="Swap" />
+      <ActionButton Icon={VscLoading} label="Bridge" />
+      <ActionButton Icon={MdOutlineArrowOutward} label="Send" />
+      <ActionButton Icon={GoLink} label="Transfer" />
+      <ActionButton Icon={FiArrowDownLeft} label="Receive" />
+      <ActionButton Icon={PiPlantLight} label="Stake" />
+      <ActionButton Icon={PiSwapLight} label="Swap" />
     </div>
   );
 }
--- src/components/Navigations/SideBar.jsx
+++ src/components/Navigations/SideBar.jsx
@@ -1,13 +1,13 @@
 import Logo from "../logo.jsx";
 import { RiNftFill } from "react-icons/ri";
-import { MdOutlineStars } from "react-icons/md";
+import { MdOutlineStars, MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { PiJoystickLight } from "react-icons/pi";
 import { LiaHubspot } from "react-icons/lia";
 import { BsGrid1X2 } from "react-icons/bs";
 import { RiTokenSwapLine } from "react-icons/ri";
 import { HiMiniArrowTopRightOnSquare } from "react-icons/hi2";
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-router-dom";
+
 const SideBar = () => {
   return (
     <div className="p-4 bg-white w-48 sticky top-0 h-screen flex flex-col justify-between">
@@ -17,45 +17,78 @@ const SideBar = () => {
 
       <div className="flex flex-col gap-4">
         <Link to="/">
-          <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
+          <div
+            className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer"
+            aria-label="Overview"
+          >
             <BsGrid1X2 className="text-xl" />
             <span>Overview</span>
           </div>
         </Link>
-        <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-          <RiTokenSwapLine className="text-xl" />
-          <span className="whitespace-nowrap flex ">
-            Move Crypto <MdOutlineKeyboardArrowRight className=" mt-1" />
-          </span>
-        </div>
 
-        <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-          <MdOutlineStars className="text-xl" />
-          <span>Token</span>
-        </div>
+        <Link to="/move-crypto">
+          <div
+            className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer"
+            aria-label="Move Crypto"
+          >
+            <RiTokenSwapLine className="text-xl" />
+            <span className="whitespace-nowrap flex">
+              Move Crypto <MdOutlineKeyboardArrowRight className="mt-1" />
+            </span>
+          </div>
+        </Link>
 
-        <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-          <RiNftFill className="text-xl" />
-          <span>NFTs</span>
-        </div>
+        <Link to="/tokens">
+          <div
+            className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer"
+            aria-label="Tokens"
+          >
+            <MdOutlineStars className="text-xl" />
+            <span>Token</span>
+          </div>
+        </Link>
 
-        <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-          <LiaHubspot className="text-xl" />
-          <span>Dapp</span>
-        </div>
+        <Link to="/nfts">
+          <div
+            className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer"
+            aria-label="NFTs"
+          >
+            <RiNftFill className="text-xl" />
+            <span>NFTs</span>
+          </div>
+        </Link>
 
-        <div className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer">
-          <PiJoystickLight className="text-xl" />
-          <span>Games</span>
-        </div>
+        <Link to="/dapp">
+          <div
+            className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer"
+            aria-label="Dapp"
+          >
+            <LiaHubspot className="text-xl" />
+            <span>Dapp</span>
+          </div>
+        </Link>
+
+        <Link to="/games">
+          <div
+            className="flex gap-2 items-center text-text hover:bg-bgColor w-40 p-2 rounded-md transition-all duration-300 cursor-pointer"
+            aria-label="Games"
+          >
+            <PiJoystickLight className="text-xl" />
+            <span>Games</span>
+          </div>
+        </Link>
       </div>
 
       <div>
         <hr className="border-gray-300 mb-4" />
 
-        <span className="flex p-2 gap-2 text-sm text-text">
+        <a
+          href="/terms-of-use"
+          className="flex p-2 gap-2 text-sm text-text hover:underline"
+          aria-label="Terms of Use"
+        >
           Terms of Use <HiMiniArrowTopRightOnSquare className="mt-1" />
-        </span>
+        </a>
       </div>
     </div>
   );
