b40becba4fd5c02244a30600b2c3d3af56419433
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-03 17:56:09 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Overview from "./pages/Overview";
 import Error from "./pages/404";
 import Buy from "./pages/Buy";
+import Sell from "./pages/Sell";
 //import SideBar from "./components/Navigations/SideBar";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/" element={<Overview />} />
           <Route path="/error" element={<Error />} />
           <Route path="/buy" element={<Buy />} />
+          <Route path="/sell" element={<Sell />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Sell.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import SideBar from "../components/Navigations/SideBar";
+import NavBar from "../components/Navigations/NavBar";
+
+import MoveCrypto from "../components/Navigations/MoveCryptoSidebar";
+
+const Sell = () => {
+  const [showMoveCryptoSidebar, setShowMoveCryptoSidebar] = useState(false);
+
+  const handleMoveCryptoClick = () => {
+    setShowMoveCryptoSidebar(!showMoveCryptoSidebar);
+  };
+
+  return (
+    <div className="flex flex-row gap-4">
+      <div className="basis-1/4 md:basis-1/6 flex">
+        <SideBar
+          showMoveCryptoSidebar={showMoveCryptoSidebar}
+          onMoveCryptoClick={handleMoveCryptoClick}
+        />
+        {showMoveCryptoSidebar && <MoveCrypto />}
+      </div>
+
+      <div className="basis-3/4 md:basis-5/6">
+        <NavBar>Sell</NavBar>
+      </div>
+    </div>
+  );
+};
+
+export default Sell;
