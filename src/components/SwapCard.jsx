8dd6e4b619221939ba1d711cdf078660bc7f62ea
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-04 14:35:10 +0100

    delete 1 file and update 2 files

--- src/components/SwapCard.jsx
+++ src/components/SwapCard.jsx
@@ -1,10 +1,16 @@
-import CoinSelect from "./CoinSelect";
+import CoinSelect from "./SwapPage/CoinSelect";
+import SwapFrom from "./SwapPage/SwapFrom";
+import { LuArrowDownUp } from "react-icons/lu";
 
 const SwapCard = () => {
   return (
     <div className="w-1/2 flex flex-col justify-center items-center">
       <CoinSelect />
+      <span>
+        <LuArrowDownUp className="text-base font-bold text-end" />
+      </span>
       <span className="text-base font-bold self-start">Swap from</span>
+      <SwapFrom />
     </div>
   );
 };
---
+++ src/components/SwapPage/SwapFrom.jsx
@@ -0,0 +1,23 @@
+import { FaEthereum } from "react-icons/fa";
+import { RxCaretDown } from "react-icons/rx";
+
+const SwapFrom = () => {
+  return (
+    <div className="border border-text rounded-lg flex items-center">
+      <div className="border-r-2 border-r-text w-40 flex justify-between p-4 font-medium text-xl items-center">
+        <FaEthereum className="w-8 h-8 text-black" />
+        ETH <RxCaretDown className="w-8 h-8 text-text" />
+      </div>
+      <div className="w-40">
+        <input
+          type="number"
+          className="p-4 focus:outline-none w-full placeholder:text-text h-14"
+          placeholder="0"
+          aria-label="Amount to Swap"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SwapFrom;
