ed56d04bfd50c6a488ee056f2e6a6e511c1b94fb
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-05 21:01:35 +0100

    update CardOne.jsx

--- src/components/StalkPage/Pool/CardOne.jsx
+++ src/components/StalkPage/Pool/CardOne.jsx
@@ -1,13 +1,13 @@
 import { CiCircleQuestion } from "react-icons/ci";
+import { FaEthereum } from "react-icons/fa";
 import { GoDotFill } from "react-icons/go";
 
 const CardOne = () => {
   return (
     <div>
-      <div className="">
-        <div className="">
-          {" "}
-          <GoDotFill />
+      <div className=" flex justify-between">
+        <div className=" flex p-2 text-sm">
+          <GoDotFill className="text-red-500" />
           Not Connected
         </div>
         <div className="">
@@ -16,9 +16,19 @@ const CardOne = () => {
       </div>
       <div className="">
         <span className="text-sm font-bold self-start">Receive To</span>
-        <div className="">
-          <div className=""></div>
-          <input type="text" placeholder=" 0 ETH" />
+        <div className=" flex border border-text p-2">
+          <div className=" flex">
+            <div className="">
+              <FaEthereum className="h-6 w-6" />
+            </div>
+            <div className="">
+              <input
+                type="text"
+                placeholder=" 0 ETH"
+                className=" focus:outline-none"
+              />
+            </div>{" "}
+          </div>
           <div className="">$0.00</div>
         </div>
       </div>
